edd9c1dbd60e8cc8ae33e2c3abff0de3eb371614
tyulyukov <makstyulyukov@gmail.com>
2022-02-02 23:46:12 +0200

    footer changed

--- Toyota/Startup.cs
+++ Toyota/Startup.cs
@@ -8,9 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Toyota.Data;
 
 namespace Toyota
