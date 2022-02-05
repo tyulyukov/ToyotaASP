a74d83d82d2ca5496fe18c33a541aff49a3db3fb
tyulyukov <makstyulyukov@gmail.com>
2022-02-05 22:25:36 +0200

    html email send

--- Toyota/Controllers/Api/StoreCallBacksController.cs
+++ Toyota/Controllers/Api/StoreCallBacksController.cs
@@ -24,7 +24,7 @@ namespace Toyota.Controllers.Api
             await _context.SaveChangesAsync();
 
             Helpers.Notification.Telegram.Send($"Заявка от {callBack.Name}\nНомер телефона: {callBack.Phone}\nID заявки: {callBack.Id}\n\nУ вас 30 секунд!");
-            Helpers.Notification.Email.Send($"Заявка от {callBack.Name}\nНомер телефона: {callBack.Phone}\nID заявки: {callBack.Id}", "Заявка на звонок", "makstyulyukov@gmail.com");
+            Helpers.Notification.Email.Send(callBack, "Заявка на звонок", "makstyulyukov@gmail.com");
 
             return CreatedAtAction("GetCallBack", new { id = callBack.Id }, callBack);
         }
--- Toyota/Helpers/Notification/Email.cs
+++ Toyota/Helpers/Notification/Email.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using MailKit.Net.Smtp;
 using MimeKit;
+using Toyota.Data.Entities;
 
 namespace Toyota.Helpers.Notification
 {
     public static class Email
     {
-        public static bool Send(String text, String subject, String email)
+        public static bool Send(CallBack callback, String subject, String email)
         {
             MimeMessage message = new MimeMessage();
 
@@ -17,7 +18,17 @@ namespace Toyota.Helpers.Notification
             message.To.Add(to);
 
             BodyBuilder bodyBuilder = new BodyBuilder();
-            bodyBuilder.TextBody = text;
+            bodyBuilder.HtmlBody = $"<div style='width: 100%; padding: 20px; background-color: gray;'>" +
+                                   $"   <img style='width: 70px;' src='https://www.pngall.com/wp-content/uploads/2016/04/Toyota-Logo-PNG-Image.png'>" +
+                                   $"</div>" +
+                                   $"<div style='width: 100%; padding: 20px; background-color: #D6E7F0;'>" +
+                                   $"   Заявка на звонок от <strong>{callback.Name}</strong><br>" +
+                                   $"   Номер телефона: <strong>{callback.Phone}</strong><br>" +
+                                   $"   ID заявки: {callback.Id}" +
+                                   $"</div>" +
+                                   $"<div style='width: 100%; padding: 20px; background-color: gray;'>" +
+                                   $"© 2022 Toyota, Inc. All rights reserved." +
+                                   $"</div>";
 
             message.Subject = subject;
             message.Body = bodyBuilder.ToMessageBody();
