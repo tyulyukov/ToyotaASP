b888ea81bb2eebfc66abb27d605bc2d9f7e47bfc
tyulyukov <makstyulyukov@gmail.com>
2022-02-13 02:33:50 +0200

    jwt auth

--- Toyota/Controllers/Auth/AccountController.cs
+++ Toyota/Controllers/Auth/AccountController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using Toyota.Auth;
 using Toyota.Data;
